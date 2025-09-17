a42e93134774bf8c7be43a47a10aea548ccd2b7d
RishithSuresh <rishithsuresh10@gmail.com>
2025-09-17 21:43:04 +0530

    Bootstrap Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import MatchTracker from "./pages/MatchTracker";
+import Home from "./pages/home";
+import MatchTracker from "./pages/Matchtracker";
 import PlayerStats from "./pages/PlayerStats";
 
 
@@ -8,11 +8,15 @@ function App() {
   return (
     <Router>
       <div>
-        <h1>⚽ PitchPulse</h1>
-        <nav>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/tracker">Match Tracker</Link> |{" "}
-          <Link to="/stats">Player Stats</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">⚽ PitchPulse</a>
+              <div>
+                <Link className="nav-link d-inline text-white" to="/">Home</Link>
+                <Link className="nav-link d-inline text-white" to="/tracker">Match Tracker</Link>
+                <Link className="nav-link d-inline text-white" to="/stats">Player Stats</Link>
+              </div>
+          </div>
         </nav>
 
         <Routes>
